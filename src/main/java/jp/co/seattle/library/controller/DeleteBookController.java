ccdaaabfa4e3b1f6d94cf2a306b125e2b1c4f4dd
Eri Uchino <eri@ERIs-MacBook-Air.local>
2022-05-17 10:55:20 +0900

    task11修正

--- src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
+++ src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
@@ -41,7 +41,7 @@ public class DeleteBookController {
 
 		int count = booksService.size(bookId);
 
-		if (count == 1) {
+		if (count >= 1) {
 			model.addAttribute("ErrorMessage", "貸出中のため削除出来ません。");
 			model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
 			return "details";
